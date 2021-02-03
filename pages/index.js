90a801aab4be19c9c765075f019c21b3da3ec3e7
Mateusz Koncikowski <mkoncikowski@gmail.com>
2021-02-03 18:54:08 +0100

    Clear home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
+import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import fetchAPI from "../lib/api";
 import { map, pick, pipe } from "ramda";
@@ -9,9 +8,6 @@ export default function Home({ data }) {
 
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
       <section className={utilStyles.headingMd}>
         {JSON.stringify(selectedData, null, 2)}
         <p>[Your Self Introduction]</p>
@@ -19,9 +15,6 @@ export default function Home({ data }) {
           (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
       </section>
     </Layout>
   );
