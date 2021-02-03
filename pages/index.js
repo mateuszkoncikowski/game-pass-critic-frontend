cd6994ac48e1aed2b3afc8579cd7fc8fc2525e78
Mateusz Koncikowski <mkoncikowski@gmail.com>
2021-02-03 18:38:44 +0100

    Remove local endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,11 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
+import Layout, {siteTitle} from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
 import fetchAPI from "../lib/api";
-import { map, pick, pipe, tail } from "ramda";
+import {map, pick, pipe} from "ramda";
 
-export default function Home({ allPostsData, data }) {
+export default function Home({ data }) {
   const selectedData = pipe(map(pick(["LocalizedProperties"])))(data);
-
-  console.log(selectedData[0]["LocalizedProperties"][0]["DeveloperName"]);
 
   return (
     <Layout home>
@@ -27,31 +22,16 @@ export default function Home({ allPostsData, data }) {
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
       </section>
     </Layout>
   );
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
   const data = await fetchAPI();
   return {
     props: {
       data,
-      allPostsData,
     },
     revalidate: 5,
   };
