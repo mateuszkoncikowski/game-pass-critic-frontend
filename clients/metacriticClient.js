aad1709b51860902ff3ce7833c643b685ca9b456
Mateusz Koncikowski <mkoncikowski@gmail.com>
2021-02-04 20:39:34 +0100

    Add puppeteer no-sandbox flag

--- clients/metacriticClient.js
+++ clients/metacriticClient.js
@@ -3,7 +3,9 @@ const puppeteer = require('puppeteer')
 export async function getGameScore(gameIds) {
   try {
     let scores = []
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
     for (let i = 0; i < gameIds.length; i++) {
       const URL = `https://www.metacritic.com/game/pc/${gameIds[i]}`
       const page = await browser.newPage()
