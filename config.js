2d87c1cf1fb4590c0c0c4937c7d1a343c561b0c4
Mateusz Koncikowski <mkoncikowski@gmail.com>
2021-03-27 08:31:19 +0100

    Introduce Google Analytics

--- config.js
+++ config.js
@@ -6,4 +6,5 @@ export const config = {
   contentfulSpace: process.env.CONTENTFUL_SPACE,
   contentfulToken: process.env.CONTENTFUL_TOKEN,
   env: process.env.ENV,
+  gaId: process.env.GA_ID,
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { Helmet } from 'react-helmet'
 
+import { GA_ID } from '../constants/constants'
+
+const isProduction = process.env.ENV === 'prod'
+
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS={true}>
@@ -8,6 +12,26 @@ export default function App({ Component, pageProps }) {
         <meta charSet="utf-8" />
         <meta name="keywords" content="xbox, gamepass" />
         <title>Game Pass Critic</title>
+        {isProduction ? (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+              }}
+            />
+          </>
+        ) : null}
       </Helmet>
       <Component {...pageProps} />
     </ChakraProvider>
